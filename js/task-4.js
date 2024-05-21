172f108e791ab58d887f596c4fca0165ef621271
ElenaUM <luzunova99@gmail.com>
2024-05-21 19:19:50 +0300

    створено task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = {};
+
+  const email = e.target.email.value.trim();
+  const password = e.target.password.value.trim();
+
+  const isEmailEmpty = email === "";
+  const isPasswordEmpty = password === "";
+
+  if (isEmailEmpty || isPasswordEmpty)
+    alert("All form fields must be filled in");
+  else {
+    data.email = email;
+    data.password = password;
+    console.log(data);
+    e.target.reset();
+  }
+});
